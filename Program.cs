cedec8bec80db2e0f12122787a6091b421799b9d
lneiz <yuntsevich@ymail.com>
2023-02-11 01:32:23 +0500

    add .gitignore

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+
+
