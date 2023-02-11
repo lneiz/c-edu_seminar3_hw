acf454bb3a6a784b8196f579d819ff688e5c89f9
lneiz <yuntsevich@ymail.com>
2023-02-12 01:20:36 +0500

    #21 - done

--- Program.cs
+++ Program.cs
@@ -21,3 +21,28 @@
 // else Console.Write("Нет числа, нет и палиндрома");
 
 // Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+double DistanceBDots(int[] A, int[] B)
+{
+    int x = (B[0] - A[0]);
+    int y = (B[1] - A[1]);
+    int z = (B[2] - A[2]);
+    return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)+ Math.Pow(z, 2));
+}
+
+int[] dotA = new int[3];
+int[] dotB = new int[3];
+
+Console.WriteLine("Поочередно введите координаты для точки A, нажимая Enter после каждой ");
+for(int i = 0; i < 3; i++)
+{
+    dotA[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("Поочередно введите координаты для точки B, нажимая Enter после каждой ");
+for(int i = 0; i < 3; i++)
+{
+    dotB[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Write("Рещение " + Convert.ToString(Math.Round(DistanceBDots(dotA, dotB), 2)));
